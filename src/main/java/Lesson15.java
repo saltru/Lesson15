74f3110076ad5f57294c484f42b981cdc942564c
saltru <salt.ru@gmail.com>
2020-03-23 20:29:43 +0400

    Commit 1: Initial commit

---
+++ src/main/java/Lesson15.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Lesson15 {
+
+    public static int lilliesCount(int days) {
+        int c = 1;
+
+        if (days < 1) {
+            return 1;
+        }
+
+        //Для значений больше чем 31, происходит переполнение int
+        //Поэтому для дней >31 функция возвращает 2^30 (1073741824)
+        if (days > 31) {
+            return 1073741824;
+        }
+
+        for (int i = 0; i < days - 1; ++i) {
+            c = c * 2;
+        }
+
+        return c;
+    }
+
+    //Функция возвращает номер дня, в котором пруд будет заполнен кувшинками более чем наполовину
+    public static int daysCount(int maxLillies) {
+        int currentDay = 1;
+
+        while (lilliesCount(currentDay) < maxLillies / 2) {
+            ++currentDay;
+        }
+
+        return currentDay;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите текущий день: ");
+        int n = scanner.nextInt();
+
+        int lc = lilliesCount(n);
+        System.out.print("Количество кувшинок в день " + n + " составляет " + lc);
+
+        if (lc % 10 == 1) {
+            System.out.println(" штука");
+        } else if ((lc % 10 > 2) && (lc % 10 < 5)) {
+            System.out.println(" штуки");
+        } else {
+            System.out.println(" штук");
+        }
+
+
+    }
+
+}
---
+++ src/test/java/Lesson15Test.java
@@ -0,0 +1,66 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Lesson15Test {
+    private class TestPair {
+        TestPair (int input_data, int expected_result) {
+            this.input_data = input_data;
+            this.expected_result = expected_result;
+        }
+        public int input_data = 0;
+        public int expected_result = 0;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void lilliesCount() {
+        TestPair testPairs[] = {
+                    new TestPair(1, 1),
+                    new TestPair(4, 8),
+                    //new TestPair(0, 1),
+                    //new TestPair(-10, 1),
+                    //new TestPair(31, 1073741824),
+                    //new TestPair(32, 1073741824),
+                    //new TestPair(55, 1073741824)
+                                };
+
+        for (int i = 0; i < testPairs.length; ++i) {
+            int actual_result = Lesson15.lilliesCount(testPairs[i].input_data);
+            assertEquals(testPairs[i].expected_result, actual_result);
+        }
+
+        /*
+        int input_data[] =      {1, 4, 0, -10,         31,         32,         55};
+        int expected_result[] = {1, 8, 1,   1, 1073741824, 1073741824, 1073741824};
+
+        for (int i = 0; i < input_data.length; ++i) {
+            int actual_result = Lesson15.lilliesCount(input_data[i]);
+
+            assertEquals(expected_result[i], actual_result);
+        }
+        */
+
+    }
+
+    @Test
+    public void daysCount() {
+        TestPair testPairs[] = {
+                new TestPair(100, 7),
+                new TestPair(1073741824, 30),
+                new TestPair(2000000000, 31),
+        };
+
+        for (int i = 0; i < testPairs.length; ++i) {
+            int actual_result = Lesson15.daysCount(testPairs[i].input_data);
+            assertEquals(testPairs[i].expected_result, actual_result);
+        }
+    }
+}
